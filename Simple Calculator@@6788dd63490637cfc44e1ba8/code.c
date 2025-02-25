7060942dd9fc6581d065c4ada93c1df7a46ba3c3
DeepikaVig <deepika1064.becse24@chitkara.edu.in>
2025-02-25 20:39:56 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 int main() {
     int a,b;
-    scanf("%d %d",&a,&b);
     char c;
-    scanf("%c",&c);
+    scanf("%d %d %c",&a,&b,&c);
     if(c=='+'){
         printf("%d",a+b);
     }
