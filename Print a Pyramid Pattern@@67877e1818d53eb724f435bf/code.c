3d83ca3170674e082b10b47b72ea2b42bb7a603a
DeepikaVig <deepika1064.becse24@chitkara.edu.in>
2025-04-01 01:40:33 +0530

    Solution to Print a Pyramid Pattern

--- Print a Pyramid Pattern@@67877e1818d53eb724f435bf/code.c
+++ Print a Pyramid Pattern@@67877e1818d53eb724f435bf/code.c
@@ -1,50 +1,13 @@
-// Your code here...#include<stdio.h>
+#include<stdio.h>
 
 int main(){
-    int rows;
-    scanf("%d",&rows);
-    if(rows == 1){
+    int a;
+    scanf("%d",&a);
+    if(a == 1){
         printf("*");
-    // }
-    // else if(rows == 2){
-    //     printf("* *\n");
-    //     printf("*");
-    // }
-    // if(rows == 3){
-    //     printf("* * *\n");
-    //     printf("* *\n");
-    //     printf("*");
-    // }
-    // if(rows == 4){
-    //     printf("* * * * \n");
-    //     printf("* * *\n");
-    //     printf("* *\n");
-    //     printf("* ");
-    // }
-    // if(rows == 5){
-        
-    //     printf("* * * * * \n");
-    //     printf("* * * * \n");
-    //     printf("* * * \n");
-    //     printf("* * \n");
-    //     printf("* ");
-    // }
-    // if(rows == 6){
-    //     printf("* * * * * * \n");
-    //     printf("* * * * *  \n");
-    //     printf("* * * *  \n");
-    //     printf("* * * \n");
-    //     printf("* * \n");
-    //     printf("* ");
-    // }
-    // if(rows == 7){
-    //     printf("* * * * * * *\n");
-    //     printf("* * * * * * \n");
-    //     printf("* * * * *\n");
-    //     printf("* * * *\n");
-    //     printf("* * *\n");
-    //     printf("* * \n");
-    //     printf("*");
-    // }
+    }
+    else if(rows == 2){
+        printf(" *\n");
+        printf("***");
     }
 }
