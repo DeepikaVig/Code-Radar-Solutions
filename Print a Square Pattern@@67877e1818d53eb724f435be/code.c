7449e0a64eed5236ce20f284640ab904e35f18b3
DeepikaVig <deepika1064.becse24@chitkara.edu.in>
2025-04-01 01:50:35 +0530

    Solution to Print a Square Pattern

--- Print a Square Pattern@@67877e1818d53eb724f435be/code.c
+++ Print a Square Pattern@@67877e1818d53eb724f435be/code.c
@@ -1,1 +1,49 @@
+#include<stdio.h>
+
+int main(){
+    int a;
+    scanf("%d",&a);
+    if(a == 1){
+        printf("*");
+    }
+    else if(a == 2){
+        printf("* *\n");
+        printf("* *");
+    }
+    else if(a == 3){
+        printf("* * * \n");
+        printf("* * * \n");
+        printf("* * *");
+    }
+    else if(a == 4){
+        printf("* * * * \n");
+        printf("* * * *  \n");
+        printf("* * * * \n");
+        printf("* * * *");
+    }
+    else if(a == 5){
+        printf("    *   \n");
+        printf("   ***   \n");
+        printf("  *****  \n");
+        printf(" ******* \n");
+        printf("*********");
+    }
+    else if(a == 6){
+        printf("     *    \n");
+        printf("    ***    \n");
+        printf("   *****   \n");
+        printf("  *******  \n");
+        printf(" ********* \n");
+        printf("***********");
+    }
+    else if(a == 7){
+        printf("      *     \n");
+        printf("     ***     \n");
+        printf("    *****    \n");
+        printf("   *******   \n");
+        printf("  *********  \n");
+        printf(" *********** \n");
+        printf("*************");
+    }
+}
 // Your code here...
