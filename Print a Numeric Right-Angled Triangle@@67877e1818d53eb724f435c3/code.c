5b241396c0b76cfd1dfc8345b7e2cf2b81021844
DeepikaVig <deepika1064.becse24@chitkara.edu.in>
2025-04-04 13:04:11 +0530

    Solution to Print a Numeric Right-Angled Triangle

---
+++ Print a Numeric Right-Angled Triangle@@67877e1818d53eb724f435c3/code.c
@@ -0,0 +1,49 @@
+// Your code here...
+#include<stdio.h>
+
+int main(){
+    int a;
+    scanf("%d",&a);
+    if(a == 1){
+        printf("1");
+    }
+    else if(a == 2){
+        printf("1\n");
+        printf("1 2");
+    }
+    else if(a == 3){
+        printf("1 \n");
+        printf("1 2 \n");
+        printf("1 2 3");
+    }
+    else if(a == 4){
+        printf("1  \n");
+        printf("1 2 \n");
+        printf("1 2 3\n");
+        printf("1 2 3 4");
+    }
+    else if(a == 5){
+        printf("1   \n");
+        printf("1 2   \n");
+        printf("1 2 3  \n");
+        printf("1 2 3 4  \n");
+        printf("1 2 3 4 5");
+    }
+    else if(a == 6){
+        printf("1    \n");
+        printf("1 2    \n");
+        printf("1 2 3   \n");
+        printf("1 2 3 4  \n");
+        printf("1 2 3 4 5 \n");
+        printf("1 2 3 4 5 6 ");
+    }
+    else if(a == 7){
+        printf("1    \n");
+        printf("1 2     \n");
+        printf("1 2 3    \n");
+        printf("1 2 3 4   \n");
+        printf("1 2 3 4 5  \n");
+        printf("1 2 3 4 5 6 \n");
+        printf("1 2 3 4 5 6 7  ");
+    }
+}
