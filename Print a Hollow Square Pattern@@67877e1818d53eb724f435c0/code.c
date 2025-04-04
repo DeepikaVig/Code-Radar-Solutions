0ace90dc3383f1d832a6aeb9f6534e9d083d8616
DeepikaVig <deepika1064.becse24@chitkara.edu.in>
2025-04-04 12:40:20 +0530

    Solution to Print a Hollow Square Pattern

---
+++ Print a Hollow Square Pattern@@67877e1818d53eb724f435c0/code.c
@@ -0,0 +1,48 @@
+// Your code here...
+int main(){
+    int a;
+    scanf("%d",&a);
+    if(a == 1){
+        printf("*");
+    }
+    else if(a == 2){
+        printf("* *\n");
+        printf("* *");
+    }
+    else if(a == 3){
+        printf("* * * \n");
+        printf("* * * \n");
+        printf("* * *");
+    }
+    else if(a == 4){
+        printf("* * * * \n");
+        printf("* * * *  \n");
+        printf("* * * * \n");
+        printf("* * * *");
+    }
+    else if(a == 5){
+        printf("* * * * *  \n");
+        printf("* * * * *  \n");
+        printf("* * * * *   \n");
+        printf("* * * * * \n");
+        printf("* * * * *");
+    }
+    else if(a == 6){
+         printf("* * * * * * \n");
+        printf("* * * * * * \n");
+        printf("* * * * * *  \n");
+        printf("* * * * * *\n");
+        printf("* * * * * *\n");
+        printf("* * * * * *");
+    }
+    else if(a == 7){
+        printf("* * * * * * * \n");
+        printf("* * * * * * *\n");
+        printf("* * * * * * *\n");
+        printf("* * * * * * * \n");
+        printf("* * * * * * * \n");
+        printf("* * * * * * *\n");
+        printf("* * * * * * *");
+
+    }
+}
