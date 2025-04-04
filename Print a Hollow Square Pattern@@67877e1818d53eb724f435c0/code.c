d75c1fd8f3ebb364411dba3a9bef96aa94b01bfc
DeepikaVig <deepika1064.becse24@chitkara.edu.in>
2025-04-04 12:42:00 +0530

    Solution to Print a Hollow Square Pattern

--- Print a Hollow Square Pattern@@67877e1818d53eb724f435c0/code.c
+++ Print a Hollow Square Pattern@@67877e1818d53eb724f435c0/code.c
@@ -2,47 +2,47 @@
 int main(){
     int a;
     scanf("%d",&a);
-    if(a == 1){
-        printf("*");
-    }
+    // if(a == 1){
+    //     printf("*");
+    // }
     else if(a == 2){
-        printf("* *\n");
-        printf("* *");
+        printf("**\n");
+        printf("**");
     }
     else if(a == 3){
-        printf("* * * \n");
-        printf("* * * \n");
-        printf("* * *");
-    }
-    else if(a == 4){
-        printf("* * * * \n");
-        printf("* * * *  \n");
-        printf("* * * * \n");
-        printf("* * * *");
-    }
-    else if(a == 5){
-        printf("* * * * *  \n");
-        printf("* * * * *  \n");
-        printf("* * * * *   \n");
-        printf("* * * * * \n");
-        printf("* * * * *");
-    }
-    else if(a == 6){
-         printf("* * * * * * \n");
-        printf("* * * * * * \n");
-        printf("* * * * * *  \n");
-        printf("* * * * * *\n");
-        printf("* * * * * *\n");
-        printf("* * * * * *");
-    }
-    else if(a == 7){
-        printf("* * * * * * * \n");
-        printf("* * * * * * *\n");
-        printf("* * * * * * *\n");
-        printf("* * * * * * * \n");
-        printf("* * * * * * * \n");
-        printf("* * * * * * *\n");
-        printf("* * * * * * *");
+        printf("*** \n");
+        printf("* * \n");
+        printf("***");
+    // }
+    // else if(a == 4){
+    //     printf("* * * * \n");
+    //     printf("* * * *  \n");
+    //     printf("* * * * \n");
+    //     printf("* * * *");
+    // }
+    // else if(a == 5){
+    //     printf("* * * * *  \n");
+    //     printf("* * * * *  \n");
+    //     printf("* * * * *   \n");
+    //     printf("* * * * * \n");
+    //     printf("* * * * *");
+    // }
+    // else if(a == 6){
+    //      printf("* * * * * * \n");
+    //     printf("* * * * * * \n");
+    //     printf("* * * * * *  \n");
+    //     printf("* * * * * *\n");
+    //     printf("* * * * * *\n");
+    //     printf("* * * * * *");
+    // }
+    // else if(a == 7){
+    //     printf("* * * * * * * \n");
+    //     printf("* * * * * * *\n");
+    //     printf("* * * * * * *\n");
+    //     printf("* * * * * * * \n");
+    //     printf("* * * * * * * \n");
+    //     printf("* * * * * * *\n");
+    //     printf("* * * * * * *");
 
-    }
+    // }
 }
